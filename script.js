c096798728ec84cea5a79ec3e0a3109353005b25
victorgileau <vgileau@hotmail.com>
2023-09-27 18:19:12 -0400

    script_correctiondufrancais-fait_2023-09-27

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const chapters = {
     intro: {
         titre: 'Intro',
-        description: '\nVous êtes un des soldats d\'un escadron de soldat ramenés à la vie. \nEn plein milieu d\'une guerre contre l\'ennemi. \nLe soldat immortel a été capturé par l\'ennemi. \n \nVous avez ordre de le retrouver avec votre escadron : \nNick, Bombe, Alive et vous.',
+        description: '\nVous êtes un des soldats d\'un escadron de soldat ramené à la vie. \nEn plein milieu d\'une guerre contre l\'ennemi. \nLe soldat immortel a été capturé par l\'ennemi. \n \nVous avez ordre de le retrouver avec votre escadron : \nNick, Bombe, Alive et vous.',
         image: './assets/images/image_histoire/image_transformer/homme_militaire_de_dos-modifier.jpg',
         bouton: [
             { titre: 'Commancer', 'destination': 'choixDepart' }
@@ -10,7 +10,7 @@ const chapters = {
 
     choixDepart: {
         titre: 'Le debut de la fin',
-        description: '\nVous devez decider si vous accepter la mission ou choisicer une autre option?',
+        description: '\nVous devez décider si vous accepter la mission ou choisir une autre option?',
         image: './assets/images/image_histoire/image_transformer/commandant_militaire_homme-modifier.jpg',
         bouton: [
             { titre: 'Fuir', 'destination': 'fuirChoixDepart' },
@@ -21,7 +21,7 @@ const chapters = {
 
     fuirChoixDepart: {
         titre: 'Tuer',
-        description: '\nVous êtes tuer par l\'ennemi. Pour avoir fais une desision aussi stupide.',
+        description: '\nVous êtes tuer par l\'ennemi. Pour avoir pris une décision aussi stupide.',
         image: './assets/images/image_histoire/image_transformer/homme_militaire_tire-modifier.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
@@ -48,7 +48,7 @@ const chapters = {
 
     choixNiveauUn: {
         titre: 'Le champ de bataille',
-        description: '\nPlusieurs options s\'offre a vous pour traverser ce champ de bataille des plus devastateur, vous devez choisire un moyen de passer et vite.',
+        description: '\nPlusieurs options s\'offre a vous pour traverser ce champ de bataille des plus dévastateurs, vous devez choisir un moyen de passer et vite.',
         image: './assets/images/image_histoire/image_transformer/homme_militaire_glise-desert-modifier.jpg',
         bouton: [
             { titre: 'Foncer', 'destination': 'foncerChoixNiveauUn' },
@@ -68,7 +68,7 @@ const chapters = {
 
     trancherChoixNiveauUn: {
         titre: 'Passer par les trancher',
-        description: '\nVous perdez Bombe ayant reçu une balle perdue dans les tranches. Mais le reste de l\'escadron survie. Vous continuer sur votre lancer.',
+        description: '\nVous perdez Bombe ayant reçu une balle perdue dans les tranches. Mais le reste de l\'escadron survie. Vous continuez sur votre lancer.',
         image: './assets/images/image_histoire/image_transformer/homme_militaire_debout_redard-vers-camera-modifier.jpg',
         bouton: [
             { titre: 'Continuer', 'destination': 'finNiveauUn' }
@@ -95,7 +95,7 @@ const chapters = {
 
     choixNiveauTroix: {
         titre: 'Catastrophe',
-        description: '\nVous devez traverser vers un point plus près de l\'objectif. Mais les choses devienne difficile rapidement et vous devez sacrifier un de vos coéquipiers pour traverser les tires ennemi. Lequel choisirez-vous?',
+        description: '\nVous devez traverser vers un point plus près de l\'objectif. Mais les choses devienne difficile rapidement et vous devez sacrifier un de vos coéquipiers pour traverser les tirs ennemis. Lequel choisirez-vous?',
         image: './assets/images/image_histoire/image_transformer/homme_militaire_marche_foret .jpg',
         bouton: [
             { titre: 'Bombe', 'destination': 'bombeChoixNiveauTroix' },
@@ -106,7 +106,7 @@ const chapters = {
 
     bombeChoixNiveauTroix: {
         titre: 'Sacrifier Bombe',
-        description: '\nBombe entand vos ordre a travers les tire ennemi et courre sur le champ de bataille et puis... \n Bombe posse le pied sur une mine et dit \"merde\". Il meurt.',
+        description: '\nBombe qui comprend vos ordres à travers les tirs ennemis et courre sur le champ de bataille et puis... \n Bombe pose le pied sur une mine et dit \"merde\". Il meurt.',
         image: './assets/images/image_histoire/image_transformer/militaire_vissage_cacher_regard-de_cote-modifier.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
@@ -115,7 +115,7 @@ const chapters = {
 
     nickChoixNiveauTroix: {
         titre: 'Sacrifier Nick',
-        description: '\nVous proposer a vos coéquiper en crian : On pourrait sacrifier Nick!!! \nMais la reaction est très négative de la part de votre équipe vous êtes tuer pour vouloir tuer le farceur Nick.',
+        description: '\nVous proposer à vos coéquipiers en criant : On pourrait sacrifier Nick!!! \nMais la réaction est très négative de la part de votre équipe vous êtes tué pour vouloir tuer le farceur Nick.',
         image: './assets/images/image_histoire/image_transformer/deux_militaire-modifer.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
@@ -124,7 +124,7 @@ const chapters = {
 
     aliveChoixNiveauTroix: {
         titre: 'Sacrifier Alive',
-        description: '\nVous proposer a vos coéquiper en crian : On pourrait sacrifier Alive!!! \nMais la reaction est très positive de la part de votre équipe vous aide a convaicre Alive de se sacrifer il le fait santant le support de tous c\'est confrère soldat. La vériter c\'est qu\'il ne l\'aimais pas beaucoup.\n \n Vous vous rendez à l\'objectif avec la perte du soldat Alive qui n\'est pas une très grande perte.',
+        description: '\nVous proposer à vos coéquipiers en criant : On pourrait sacrifier Alive!!! \nMais la réaction est très positive de la part de votre équipe vous aide à convaincre Alive de se sacrifier il le fait sentant le support de tous c\'est confrère soldat. La vérité c\'est qu\'il ne l\'aimais pas beaucoup.\n \n Vous vous rendez à l\'objectif avec la perte du soldat Alive qui n\'est pas une très grande perte.',
         image: './assets/images/image_histoire/image_transformer/homme_militaire_glise-desert-modifier.jpg',
         bouton: [
             { titre: 'Continuer', 'destination': 'choixNiveauQuatre' }
@@ -133,7 +133,7 @@ const chapters = {
 
     choixNiveauQuatre: {
         titre: 'Un choix difficile...',
-        description: '\nVous devez trouver une façon de détruire l\'ennemi protégeant l\'objectif pour sécuriser la zone. \nVos coéquipiers et vous doivents trouvers une solutions, mais la quel?',
+        description: '\nVous devez trouver une façon de détruire l\'ennemi protégeant l\'objectif pour sécuriser la zone. \nVos coéquipiers et vous doivent trouver une solution, mais la quel?',
         image: './assets/images/image_histoire/image_transformer/batiment_detruit-modifier.jpg',
         bouton: [
             { titre: 'Bombe', 'destination': 'bombeChoixNiveauQuatreFin' },
@@ -144,7 +144,7 @@ const chapters = {
 
     vousChoixNiveauQuatre: {
         titre: 'Uh sacrifice pour le escadron',
-        description: '\nVous voyez tout la difficulter de la situation dans la quelle votre escadron ce trouve. Il menque de munition et le moral est bas.\nVous foncer vers l\'ennemi pour destabliser l\'ennemi et réalumer la flame de vos confrères. \n\n\nMais maleureusement vou mourrez d\'un tire a la tête en un instant.',
+        description: '\nVous voyez tout la difficulté de la situation dans laquelle votre escadron se trouve. Il manque de munition et le moral est bas.\nVous foncer vers l\'ennemi pour déstabiliser l\'ennemi et rallumer la flame de vos confrères. \n\n\nMais malheureusement vous mourrez d\'un tir à la tête en un instant.',
         image: './assets/images/image_histoire/image_transformer/homme_masque_ordre-modifier.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
@@ -153,7 +153,7 @@ const chapters = {
 
     nickChoixNiveauQuatre: {
         titre: 'Uh échec pour la gloire',
-        description: '\nNick crie qui va les sauver : Je vais tout vous sauver!!! \nNick enlève la goupille d\'une grenade, mais une blessure à son bras fait en sorte qu\'il l\'échappe, ce qui tue tout votre escadron. \nIl ne vous a pas sauver du tout.',
+        description: '\nNick crie qui va les sauver : Je vais tout vous sauver!!! \nNick enlève la goupille d\'une grenade, mais une blessure à son bras fait en sorte qu\'il l\'échappe, ce qui tue tout votre escadron. \nIl ne vous a pas sauvé du tout.',
         image: './assets/images/image_histoire/image_transformer/explosion_carre-modifier.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
@@ -162,7 +162,7 @@ const chapters = {
 
     bombeChoixNiveauQuatreFin: {
         titre: 'Uh sacrifice glorieux',
-        description: '\nBombe dit avec un ton certait : Je vais vous auver les gars. \nBombe fait le kamikaze et élimine l\'ennemi ce qui crée la zone sécurisée. \n\nL\'objectif est sauvé. Vous avez maintenant une chance de gagner la guerre avec l\'aide de l\'immortel. \n\n FIN',
+        description: '\nBombe dit avec un ton certain : Je vais vous sauver les gars. \nBombe fait le kamikaze et élimine l\'ennemi ce qui crée la zone sécurisée. \n\nL\'objectif est sauvé. Vous avez maintenant une chance de gagner la guerre avec l\'aide de l\'immortel. \n\n FIN',
         image: './assets/images/image_histoire/image_transformer/hommex2_militaire_fummee-modifer.jpg',
         bouton: [
             { titre: 'Succès - Recommancer', 'destination': 'intro' }
