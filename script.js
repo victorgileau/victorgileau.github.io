10dd783ce50d38e393689ec2ce176d8ba63bc3b2
victorgileau <vgileau@hotmail.com>
2023-10-03 09:47:16 -0400

    css_bouton_addapter_2023-10-03

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ const chapters = {
         image: './assets/images/image_histoire/image_transformer/commandant_militaire_homme-modifier.jpg',
         bouton: [
             { titre: 'Fuir', 'destination': 'fuirChoixDepart' },
-            { titre: 'Accepter', 'destination': 'refuserChoixDepart' },
-            { titre: 'Refuser', 'destination': 'accepterChoixDepart' }
+            { titre: 'Accepter', 'destination': 'accepterChoixDepart' },
+            { titre: 'Refuser', 'destination': 'refuserChoixDepart' }
         ]
     },
 
     fuirChoixDepart: {
         titre: 'Tuer',
         description: '\nVous êtes tuer par l\'ennemi. Pour avoir pris une décision aussi stupide.',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_tire-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/homme_militaire_tire-modifer.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
         ]
@@ -31,7 +31,7 @@ const chapters = {
     refuserChoixDepart: {
         titre: 'Exécuté',
         description: '\nVous avez été exécuté pour avoir désobéi un ordre direct. C\'était une certitude.',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_tire-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/homme_militaire_tire-modifer.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
         ]
@@ -49,7 +49,7 @@ const chapters = {
     choixNiveauUn: {
         titre: 'Le champ de bataille',
         description: '\nPlusieurs options s\'offre a vous pour traverser ce champ de bataille des plus dévastateurs, vous devez choisir un moyen de passer et vite.',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_glise-desert-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/homme_militaire_glise-desert-modifer.jpg',
         bouton: [
             { titre: 'Foncer', 'destination': 'foncerChoixNiveauUn' },
             { titre: 'Utiliser un véhicule', 'destination': 'vehiculeChoixNiveauUn' },
@@ -60,7 +60,7 @@ const chapters = {
     foncerChoixNiveauUn: {
         titre: 'Foncer',
         description: '\nVous êtes tuer par l\'ennemi, conseille d\'ami ne pas foncer tout droit dans un champ de bataille.',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_tire-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/homme_militaire_tire-modifer.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
         ]
@@ -69,7 +69,7 @@ const chapters = {
     trancherChoixNiveauUn: {
         titre: 'Passer par les trancher',
         description: '\nVous perdez Bombe ayant reçu une balle perdue dans les tranches. Mais le reste de l\'escadron survie. Vous continuez sur votre lancer.',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_debout_redard-vers-camera-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/millitaire_homme_debout_redard-vers-camera-modifier.jpg',
         bouton: [
             { titre: 'Continuer', 'destination': 'finNiveauUn' }
         ]
@@ -78,7 +78,7 @@ const chapters = {
     vehiculeChoixNiveauUn: {
         titre: 'Utiliser un véhicule comme bouclier.',
         description: '\nLe véhicule est une bonne défense. Tous les membres de l\'escadron survivent. Mais le véhicule ne fonctionne plus.',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_debout_redard-vers-camera-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/millitaire_homme_debout_redard-vers-camera-modifier.jpg',
         bouton: [
             { titre: 'Continuer', 'destination': 'finNiveauUn' }
         ]
@@ -96,7 +96,7 @@ const chapters = {
     choixNiveauTroix: {
         titre: 'Catastrophe',
         description: '\nVous devez traverser vers un point plus près de l\'objectif. Mais les choses devienne difficile rapidement et vous devez sacrifier un de vos coéquipiers pour traverser les tirs ennemis. Lequel choisirez-vous?',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_marche_foret .jpg',
+        image: './assets/images/image_histoire/image_transformer/homme_militaire_marche_foret.jpg',
         bouton: [
             { titre: 'Bombe', 'destination': 'bombeChoixNiveauTroix' },
             { titre: 'Nick', 'destination': 'nickChoixNiveauTroix' },
@@ -107,7 +107,7 @@ const chapters = {
     bombeChoixNiveauTroix: {
         titre: 'Sacrifier Bombe',
         description: '\nBombe qui comprend vos ordres à travers les tirs ennemis et courre sur le champ de bataille et puis... \n Bombe pose le pied sur une mine et dit \"merde\". Il meurt.',
-        image: './assets/images/image_histoire/image_transformer/militaire_vissage_cacher_regard-de_cote-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/militaire_vissage_cacher_regard-de_cote-modifer.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
         ]
@@ -116,7 +116,7 @@ const chapters = {
     nickChoixNiveauTroix: {
         titre: 'Sacrifier Nick',
         description: '\nVous proposer à vos coéquipiers en criant : On pourrait sacrifier Nick!!! \nMais la réaction est très négative de la part de votre équipe vous êtes tué pour vouloir tuer le farceur Nick.',
-        image: './assets/images/image_histoire/image_transformer/deux_militaire-modifer.jpg',
+        image: './assets/images/image_histoire/image_transformer/deux_militaire-modifier.jpg',
         bouton: [
             { titre: 'Recommancer', 'destination': 'intro' }
         ]
@@ -125,7 +125,7 @@ const chapters = {
     aliveChoixNiveauTroix: {
         titre: 'Sacrifier Alive',
         description: '\nVous proposer à vos coéquipiers en criant : On pourrait sacrifier Alive!!! \nMais la réaction est très positive de la part de votre équipe vous aide à convaincre Alive de se sacrifier il le fait sentant le support de tous c\'est confrère soldat. La vérité c\'est qu\'il ne l\'aimais pas beaucoup.\n \n Vous vous rendez à l\'objectif avec la perte du soldat Alive qui n\'est pas une très grande perte.',
-        image: './assets/images/image_histoire/image_transformer/homme_militaire_glise-desert-modifier.jpg',
+        image: './assets/images/image_histoire/image_transformer/homme_militaire_glise-desert-modifer.jpg',
         bouton: [
             { titre: 'Continuer', 'destination': 'choixNiveauQuatre' }
         ]
@@ -163,7 +163,7 @@ const chapters = {
     bombeChoixNiveauQuatreFin: {
         titre: 'Uh sacrifice glorieux',
         description: '\nBombe dit avec un ton certain : Je vais vous sauver les gars. \nBombe fait le kamikaze et élimine l\'ennemi ce qui crée la zone sécurisée. \n\nL\'objectif est sauvé. Vous avez maintenant une chance de gagner la guerre avec l\'aide de l\'immortel. \n\n FIN',
-        image: './assets/images/image_histoire/image_transformer/hommex2_militaire_fummee-modifer.jpg',
+        image: './assets/images/image_histoire/image_transformer/hommex2_miltaire_fumee-modifer.jpg',
         bouton: [
             { titre: 'Succès - Recommancer', 'destination': 'intro' }
         ]
@@ -177,6 +177,7 @@ const title = document.querySelector('#titre');
 const chapterDescription = document.querySelector('p');
 const imageChapter = document.querySelector('.image');
 const containButton = document.querySelector('.boutons');
+let isBombeDead = false;
 
 /*
 function goToChapter(chapter) {
@@ -225,3 +226,18 @@ function goToChapter(chapter) {
 }
 
 goToChapter('intro');
+
+/*
+if (goToChapter('bombeChoixNiveauTroix') || goToChapter('trancherChoixNiveauUn')) {
+    isBombeDead = true;
+}
+
+else if (goToChapter('trancherChoixNiveauUn')) {
+    isBombeDead = true;
+}
+
+if (isBombeDead == true) {
+    chapters.bombeChoixNiveauTroix.bouton.shift();
+    chapters.choixNiveauQuatre.bouton.shift();
+}
+*/
